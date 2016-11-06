0d873017c5d50c84844d57150a7e206d37e644f0
Thor Erik Lie <thor@thorerik.com>
2016-11-06 11:19:27 +0100

    tests

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoSearch = require('TodoSearch');
+
+describe('TodoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toExist();
+  });
+
+  it('should call onSearch if something is entered', () => {
+    var searchText = 'Meow';
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    todoSearch.refs.searchText.value = searchText;
+    TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, searchText);
+  });
+
+  it('should call onSearch if showCompleted is checked', () => {
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    todoSearch.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, '');
+  });
+});
