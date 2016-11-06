e37bc8f4889aa9d44194284b2c2e53d63748a5b9
Thor Erik Lie <thor@thorerik.com>
2016-11-06 19:36:23 +0100

    Filter todos and sort them

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -9,7 +9,7 @@ module.exports = {
     getTodos: function() {
         var stringTodos = localStorage.getItem('todos');
         var todos = [];
-        
+
         try {
             todos = JSON.parse(stringTodos);
         } catch (e) {
@@ -17,5 +17,32 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+      var filteredTodos = todos;
+
+      // filter by showCompleted
+      filteredTodos = filteredTodos.filter((todo) => {
+        return !todo.completed || showCompleted
+      });
+
+      // filter by searchText
+      filteredTodos = filteredTodos.filter((todo) => {
+        var text = todo.text.toLowerCase();
+        return searchText.length === 0 || text.indexOf(searchText) > -1;
+      });
+
+      // sort todos with non-completed first
+      filteredTodos.sort((a,b) => {
+        if (!a.completed && b.completed) {
+          return -1;
+        } else if (a.completed && !b.completed) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      return filteredTodos;
     }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -18,7 +18,7 @@ describe('TodoAPI', () => {
                 text: "meow",
                 completed: false
             }];
-            
+
             TodoAPI.setTodos(todos);
 
             var actualTodos = JSON.parse(localStorage.getItem('todos'));
@@ -54,5 +54,51 @@ describe('TodoAPI', () => {
 
             expect(actualTodos).toEqual(todos);
         });
+    });
+
+    describe('filterTodos', () => {
+      var todos = [
+        {
+          id: 1,
+          text: 'Meow',
+          completed: true
+        },
+        {
+          id: 2,
+          text: 'Woof',
+          completed: false
+        },
+        {
+          id: 3,
+          text: 'Mrow',
+          completed: true
+        }
+      ];
+
+      it('should return all items if showCompleted is true', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
+      it('should return only uncompleted items if showCompleted is false', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+        expect(filteredTodos.length).toBe(1);
+      });
+
+      it('should sort by completed status', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+      });
+
+      it('should filter todos by searchText', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, 'm');
+        expect(filteredTodos.length).toBe(2);
+      });
+
+      it('should return all todos if searchText is empty', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
     });
 });
