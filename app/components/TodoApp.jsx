b904d1ec2b4a35a2182dc984326cd50e6479c07d
Thor Erik Lie <thor@thorerik.com>
2016-11-06 09:58:17 +0100

    basic structure

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
 
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Meow'
+        },
+        {
+          id: 2,
+          text: 'Woof'
+        },
+        {
+          id: 3,
+          text: 'Mrow'
+        },
+        {
+          id: 4,
+          text: 'Mooo'
+        }
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
     return (
       <div>
-        <p>Todo App</p>
+        <TodoList todos={todos}/>
       </div>
     );
   }
